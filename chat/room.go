1d9bd110e070358ded0b57d60a5bb49c18e09925
Ting Hsu <s403160087@gmail.com>
2022-07-23 23:36:59 +0800

    new channel for join & leave clients, run func added

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,29 @@
 package main
 
 type room struct {
+	// holds incoming msg
 	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	// all current clinets in this room
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		// if there's a client wants to join, save it to map
+		case client := <-r.join:
+			r.clients[client] = true
+		// if wants to move, delete the client from map & close the client's send channel
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		// if there's msg wants to send in to this room, add them to send channels of all client
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
 }
