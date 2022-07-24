daa98ab02ea093911fd1ff39c8a381465904ce8a
Ting Hsu <s403160087@gmail.com>
2022-07-25 00:18:33 +0800

    add traceOff in room type & remove New in main

--- chat/main.go
+++ chat/main.go
@@ -5,10 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
 	"sync"
-	"webServer/trace"
 )
 
 type templateHandler struct {
@@ -28,7 +26,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
 	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
+	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
